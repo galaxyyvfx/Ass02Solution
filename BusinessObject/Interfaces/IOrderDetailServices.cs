3e184c067a184cfe12178132cdd644c2ebc38e9e
Quoc Trung <tphcmtrung@gmail.com>
2022-06-28 21:24:29 +0700

    OrderDetailServices

--- BusinessObject/Interfaces/IOrderDetailServices.cs
+++ BusinessObject/Interfaces/IOrderDetailServices.cs
@@ -7,7 +7,7 @@ public interface IOrderDetailServices
     public void AddOrderDetail(OrderDetail orderDetail);
     public void UpdateOrderDetail(OrderDetail orderDetail);
     public void DeleteOrderDetail(OrderDetail orderDetail);
-    public OrderDetail GetOrderDetail(int id);
+    public OrderDetail GetOrderDetail(int productId, int orderId);
     public IEnumerable<OrderDetail> GetOrderDetailList();
     public IEnumerable<OrderDetail> GetOrderDetailFromOrderId(int id);
     public decimal GetTotalPriceFromOrderId(int id);
--- BusinessObject/OrderDetailServices.cs
+++ BusinessObject/OrderDetailServices.cs
@@ -6,36 +6,116 @@ public class OrderDetailServices : IOrderDetailServices
 {
     public void AddOrderDetail(OrderDetail orderDetail)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            dbContext.OrderDetails.Add(orderDetail);
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
     public void DeleteOrderDetail(OrderDetail orderDetail)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            var od =
+                dbContext.OrderDetails
+                .SingleOrDefault
+                (c => c.OrderId == orderDetail.OrderId 
+                && c.ProductId == orderDetail.ProductId);
+            dbContext.OrderDetails.Remove(od);
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 
-    public OrderDetail GetOrderDetail(int id)
+    public OrderDetail GetOrderDetail(int productId, int orderId)
     {
-        throw new NotImplementedException();
+        OrderDetail orderDetail = null;
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            orderDetail = dbContext.OrderDetails
+                .Single(c => c.OrderId == orderDetail.OrderId
+                && c.ProductId == orderDetail.ProductId);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        return orderDetail;
     }
 
     public IEnumerable<OrderDetail> GetOrderDetailFromOrderId(int id)
     {
-        throw new NotImplementedException();
+        IEnumerable<OrderDetail> list = null;
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            list = dbContext.OrderDetails
+                .Where(o => o.OrderId == id)
+                .ToList();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        return list;
     }
 
     public IEnumerable<OrderDetail> GetOrderDetailList()
     {
-        throw new NotImplementedException();
+        IEnumerable<OrderDetail> list = null;
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            list = dbContext.OrderDetails
+                .ToList();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        return list;
     }
 
     public decimal GetTotalPriceFromOrderId(int id)
     {
-        throw new NotImplementedException();
+        decimal result = 0;
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            result = (decimal)dbContext.OrderDetails
+                .Where(o => o.OrderId == id)
+                .Sum(p => p.Quantity * p.UnitPrice);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        return result;
     }
 
     public void UpdateOrderDetail(OrderDetail orderDetail)
     {
-        throw new NotImplementedException();
+        try
+        {
+            using FStoreDBContext dbContext = new FStoreDBContext();
+            dbContext.Entry<OrderDetail>(orderDetail).State =
+                Microsoft.EntityFrameworkCore.EntityState.Modified;
+            dbContext.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
     }
 }
