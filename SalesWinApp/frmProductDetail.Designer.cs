fc3176203325fda73684ac5eee27bf84cdbb9a28
Tuan Kiet <Kietpttse161220@fpt.edu.vn>
2022-06-29 19:12:35 +0700

    frmProducts

---
+++ SalesWinApp/frmProductDetail.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace SalesWinApp
+{
+    partial class frmProductDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.lblCategoryID = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblUnitPrice = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.numUnitPrice = new System.Windows.Forms.NumericUpDown();
+            this.numUnitsInStock = new System.Windows.Forms.NumericUpDown();
+            this.numCategoryID = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numUnitPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUnitsInStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCategoryID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Location = new System.Drawing.Point(22, 39);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(79, 20);
+            this.lblProductID.TabIndex = 0;
+            this.lblProductID.Text = "Product ID";
+            // 
+            // lblCategoryID
+            // 
+            this.lblCategoryID.AutoSize = true;
+            this.lblCategoryID.Location = new System.Drawing.Point(22, 79);
+            this.lblCategoryID.Name = "lblCategoryID";
+            this.lblCategoryID.Size = new System.Drawing.Size(88, 20);
+            this.lblCategoryID.TabIndex = 1;
+            this.lblCategoryID.Text = "Category ID";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(22, 119);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(104, 20);
+            this.lblProductName.TabIndex = 2;
+            this.lblProductName.Text = "Product Name";
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Location = new System.Drawing.Point(22, 159);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(56, 20);
+            this.lblWeight.TabIndex = 3;
+            this.lblWeight.Text = "Weight";
+            // 
+            // lblUnitPrice
+            // 
+            this.lblUnitPrice.AutoSize = true;
+            this.lblUnitPrice.Location = new System.Drawing.Point(22, 199);
+            this.lblUnitPrice.Name = "lblUnitPrice";
+            this.lblUnitPrice.Size = new System.Drawing.Size(72, 20);
+            this.lblUnitPrice.TabIndex = 4;
+            this.lblUnitPrice.Text = "Unit Price";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(22, 239);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(98, 20);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Units In Stock";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Enabled = false;
+            this.txtProductID.Location = new System.Drawing.Point(152, 36);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(231, 27);
+            this.txtProductID.TabIndex = 6;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(152, 116);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(231, 27);
+            this.txtProductName.TabIndex = 8;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(152, 156);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(231, 27);
+            this.txtWeight.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(87, 286);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(229, 286);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 29);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // numUnitPrice
+            // 
+            this.numUnitPrice.Location = new System.Drawing.Point(152, 197);
+            this.numUnitPrice.Name = "numUnitPrice";
+            this.numUnitPrice.Size = new System.Drawing.Size(231, 27);
+            this.numUnitPrice.TabIndex = 14;
+            // 
+            // numUnitsInStock
+            // 
+            this.numUnitsInStock.Location = new System.Drawing.Point(152, 237);
+            this.numUnitsInStock.Name = "numUnitsInStock";
+            this.numUnitsInStock.Size = new System.Drawing.Size(231, 27);
+            this.numUnitsInStock.TabIndex = 15;
+            // 
+            // numCategoryID
+            // 
+            this.numCategoryID.Location = new System.Drawing.Point(152, 77);
+            this.numCategoryID.Name = "numCategoryID";
+            this.numCategoryID.Size = new System.Drawing.Size(231, 27);
+            this.numCategoryID.TabIndex = 16;
+            // 
+            // frmMemberDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 341);
+            this.Controls.Add(this.numCategoryID);
+            this.Controls.Add(this.numUnitsInStock);
+            this.Controls.Add(this.numUnitPrice);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUnitPrice);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.lblCategoryID);
+            this.Controls.Add(this.lblProductID);
+            this.Name = "frmMemberDetail";
+            this.Text = "Product Details";
+            ((System.ComponentModel.ISupportInitialize)(this.numUnitPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUnitsInStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCategoryID)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblProductID;
+        private Label lblCategoryID;
+        private Label lblProductName;
+        private Label lblWeight;
+        private Label lblUnitPrice;
+        private Label lblPassword;
+        private TextBox txtProductID;
+        private TextBox txtProductName;
+        private TextBox txtWeight;
+        private Button btnSave;
+        private Button btnCancel;
+        private NumericUpDown numUnitPrice;
+        private NumericUpDown numUnitsInStock;
+        private NumericUpDown numCategoryID;
+    }
+}
