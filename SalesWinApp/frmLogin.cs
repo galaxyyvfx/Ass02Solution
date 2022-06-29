2d6dd9b7685d4af795625a917cdff954c6a077cb
Quoc Trung <tphcmtrung@gmail.com>
2022-06-29 16:13:38 +0700

    frmLogin

--- SalesWinApp/frmLogin.cs
+++ SalesWinApp/frmLogin.cs
@@ -1,5 +1,6 @@
 using BusinessObject;
 using BusinessObject.Interfaces;
+using DataAccess.Models;
 
 namespace SalesWinApp;
 
@@ -9,5 +10,21 @@ public partial class frmLogin : Form
     public frmLogin()
     {
         InitializeComponent();
+    }
+
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        string email    = txtEmail.Text;
+        string password = txtPassword.Text;
+
+        try
+        {
+            Member loginMember = memberServices.Login(email, password);
+            frmMain frmMain = new frmMain(loginMember);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Login");
+        }
     }
 }
